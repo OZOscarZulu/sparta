463808a3586cf431850a37731a427a54c5b4c71b
OZOscarZulu <ktstyle83@gmail.com>
2020-05-22 20:03:50 +0900

    Initial commit

---
+++ etc/test.js
@@ -0,0 +1,38 @@
+//1. numbers를 for문 돌면서 값을 출력하기
+
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for (let i = 0; i < 10; i++) {
+    console.log(numbers[10])
+}
+
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i])
+}
+
+
+//2. 짝수 찾기
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 == 0) {
+        console.log(numbers[i])
+    }
+}
+
+
+//3. number를 받아서 그보다 작은 숫자를 모두 출력한다.
+function print_all_number(num) {
+    for (let i = 0; i < num; i++) {
+        console.log(number[i])
+    }
+}
+//아래 코드를 실행
+print_all_number(5)
+
+
+//4. number
+
+
+
+
---
+++ etc/test1.js
@@ -0,0 +1,5 @@
+let max_i = 0;
+let mise_list = response["RealtimeCityAir"]["row"];
+for (let i = 0; i < mise_list.length; i++) {
+    let mise = mise_list[i];
+    if (mse_list)
